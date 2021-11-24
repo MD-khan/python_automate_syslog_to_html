159af0b56c79e0dfe5cc9a6e70979e8ce675f215
MD-khan <md.monir.khan707@gmail.com>
2021-11-24 00:24:11 -0500

    Create ticky_check.py
    Here's your challenge: Write a script to generate two different reports based on the ranking of errors generated by the system and the user usage statistics for the service. You'll write the script on your own, but we'll guide you throughout.
    
    First, import all the Python modules that you'll use in this Python script. After importing the necessary modules, initialize two dictionaries: one for the number of different error messages and another to count the number of entries for each user (splitting between INFO and ERROR).
    
    Now, parse through each log entry in the syslog.log file by iterating over the file.
    
    For each log entry, you'll have to first check if it matches the INFO or ERROR message formats. You should use regular expressions for this. When you get a successful match, add one to the corresponding value in the per_user dictionary. If you get an ERROR message, add one to the corresponding entry in the error dictionary by using proper data structure.
    
    After you've processed the log entries from the syslog.log file, you need to sort both the per_user and error dictionary before creating CSV report files.
    
    Keep in mind that:
    
    The error dictionary should be sorted by the number of errors from most common to least common.
    The user dictionary should be sorted by username.
    Insert column names as ("Error", "Count") at the zero index position of the sorted error dictionary. And insert column names as ("Username", "INFO", "ERROR") at the zero index position of the sorted per_user dictionary.
    
    After sorting these dictionaries, store them in two different files: error_message.csv and user_statistics.csv.

---
+++ ticky_check.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import re
+import csv
+import operator
+
+file = open("syslog.log")
+lines = file.readlines()
+file.close
+
+per_user = {}
+error_detais = {}
+
+for line in lines:
+    result = re.search(
+        r"ticky: (ERROR|INFO) ([\w ]*) [\[\]#\d\s]*\((\w*)", line)
+    if result is not None:
+        # Count the Error frequency
+        if result.group(1) == "ERROR":  # result[1] - ERROR, INFO
+            # result[2] is message
+            if result.group(2) not in error_detais.keys():
+                error_detais[result.group(2)] = 0
+            error_detais[result.group(2)] += 1
+
+        if result.group(3) not in per_user.keys():  # result[3] - user name
+            per_user[result.group(3)] = {}
+            per_user[result.group(3)]["INFO"] = 0
+            per_user[result.group(3)]["ERROR"] = 0
+
+        if result.group(1) == "ERROR":
+            per_user[result.group(3)]["ERROR"] += 1
+        elif result.group(1) == "INFO":
+            per_user[result.group(3)]["INFO"] += 1
+
+# print(per_user)
+error_detais = sorted(error_detais.items(),
+                      key=operator.itemgetter(1), reverse=True)
+
+with open("error_message.csv", "w") as file:
+    write = csv.writer(file)
+    write.writerow(["Error", "Count"])
+    write.writerows(error_detais)
+file.close()
+
+per_user = sorted(per_user.items())
+with open("user_statistics.csv", 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(["Username", "INFO", "ERROR"])
+    for line in per_user: 
+        writer.writerow([line[0], line[1]["INFO"], line[1]["ERROR"]])
+f.close()
